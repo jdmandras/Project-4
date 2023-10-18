0d4debd70b58544dbf97b4104047fd5659c11123
VickyL86 <lasotaludwika1@gmail.com>
2023-10-17 19:51:49 -0500

    Deleted a note from SQL schema

--- SQL/SupplyChain_Schema.sql
+++ SQL/SupplyChain_Schema.sql
@@ -1,6 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/dK2y4Z
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+﻿
 
 
 CREATE TABLE "customers" (
